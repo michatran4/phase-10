0326103a8ea5f6750e01150022d0e360ea4f2a51
Joharah <arah123seguerra@gmail.com>
2022-01-24 09:13:05 -0600

    Added side menu
    side menu for scoreboard and instructions.

--- src/GUI.java
+++ src/GUI.java
@@ -3,68 +3,93 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 public class GUI {
-    JFrame frame;
-    JPanel topPanel, leftPanel, rightPanel, botPanel, centerPanel, centerLeftPanel, centerRightPanel;
-    JButton drawPile, discardPile;
+    private JFrame frame;
+    private JPanel boardPanel, menuPanel, topPanel, leftPanel, rightPanel, botPanel, centerPanel, centerLeftPanel, centerRightPanel;
+    private JButton drawPile, discardPile;
+
+    private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private final Dimension boardSize = new Dimension((int)((7)*(screenSize.getWidth()/8)), (int)screenSize.getHeight());
+    private final Dimension menuSize = new Dimension((int)(screenSize.getWidth()/8), screenSize.height);
 
     //Basic setup of the frame container, panels, cards piles (buttons)
     public GUI()
     {
-        //Set up main frame aka main container that holds all panels and utilizes borderlayout manager
+        //Set up main container frame of all panels; utilizes borderLayout manager
         frame = new JFrame("Phase 10");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize(screenSize);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setLayout(new BorderLayout());
 
+
+        //Set up parent panels; Left = Board, Right = Menu for scoreboard and rules
+        //boardPanel is where game takes place, parent panel for player panels which hold cards
+        boardPanel = new JPanel();
+        boardPanel.setBackground(Color.BLACK);
+        boardPanel.setLayout(new BorderLayout());
+        boardPanel.setPreferredSize(boardSize);
+
+        menuPanel = new JPanel();
+        menuPanel.setBackground(Color.white);
+        Dimension menuSize = new Dimension((int)(screenSize.getWidth()/8), screenSize.height);
+        menuPanel.setPreferredSize(menuSize);
+
+        frame.add(boardPanel, BorderLayout.CENTER);
+        frame.add(menuPanel, BorderLayout.EAST);
+
+
         //Set up panels that will hold cards of players using __layout manager
+        int top_botPanelWidth = (int)(6*(boardSize.getWidth()/7));
+        int top_botPanelHeight = (int)(boardSize.getHeight()/7);
+        int sidePanelsWidth = (int)(boardSize.getHeight()/7);
+        int sidePanelsHeight = (int)(6*(boardSize.getWidth()/7));
+
         botPanel = new JPanel(); //Contains Player 1, aka person playing
         botPanel.setBackground(Color.RED);
-        botPanel.setPreferredSize(new Dimension((int) screenSize.getWidth()-50, 200));
+        botPanel.setPreferredSize(new Dimension(top_botPanelWidth, (int)((1.75)*top_botPanelHeight)));
 
         rightPanel = new JPanel(); //Contains Player 2 cards, CPU
         rightPanel.setBackground(Color.BLUE);
-        rightPanel.setPreferredSize(new Dimension(150,(int) screenSize.getHeight()-50));
+        rightPanel.setPreferredSize(new Dimension(sidePanelsWidth, sidePanelsHeight));
 
         topPanel = new JPanel(); //Contains Player 3 cards, CPU
         topPanel.setBackground(Color.GREEN);
-        topPanel.setPreferredSize(new Dimension((int) screenSize.getWidth()-50, 100));
+        topPanel.setPreferredSize(new Dimension(top_botPanelWidth, top_botPanelHeight));
 
         leftPanel = new JPanel(); //Contains Player 4 cards, CPU
         leftPanel.setBackground(Color.MAGENTA);
-        leftPanel.setPreferredSize(new Dimension(150,(int) screenSize.getHeight()-50));
+        leftPanel.setPreferredSize(new Dimension(sidePanelsWidth, sidePanelsHeight));
 
         centerPanel = new JPanel(); //Contains the draw and discard piles (centerLeft) AND completed phase sets (centerRight)
+        centerPanel.setLayout(new BorderLayout());
         centerPanel.setBackground(Color.DARK_GRAY);
 
-        frame.add(botPanel, BorderLayout.SOUTH);
-        frame.add(rightPanel, BorderLayout.EAST);
-        frame.add(topPanel, BorderLayout.NORTH);
-        frame.add(leftPanel, BorderLayout.WEST);
-        frame.add(centerPanel, BorderLayout.CENTER);
+        boardPanel.add(botPanel, BorderLayout.SOUTH);
+        boardPanel.add(rightPanel, BorderLayout.EAST);
+        boardPanel.add(topPanel, BorderLayout.NORTH);
+        boardPanel.add(leftPanel, BorderLayout.WEST);
+        boardPanel.add(centerPanel, BorderLayout.CENTER);
 
-        //Set up draw and discard pile
-        //NOTE: centerPanel contains 2 more JPanels; left for the discard and draw piles, right for the completed phase sets
-        centerPanel.setLayout(new BorderLayout());
 
+        //NOTE: centerPanel contains 2 more JPanels; Left = discard and draw piles, Right = completed phase sets
+        //Set up draw & discard pile panel
         centerLeftPanel = new JPanel();
         centerLeftPanel.setBackground(Color.DARK_GRAY);
-        centerLeftPanel.setPreferredSize(new Dimension((int) screenSize.getWidth()-1100,(int) screenSize.getHeight()-150));
-        centerPanel.add(centerLeftPanel, BorderLayout.WEST);
+        centerLeftPanel.setPreferredSize(new Dimension((int)(boardSize.getWidth()/3),(int) boardSize.getHeight()));
+        centerLeftPanel.setLayout(new FlowLayout());
 
-        /*
         drawPile = new JButton("Draw");
-        drawPile.setSize(20,40);
+        drawPile.setPreferredSize(new Dimension(90,120));
         centerLeftPanel.add(drawPile);
-         */
 
+        //Set up completed phase sets panel
         centerRightPanel = new JPanel();
         centerRightPanel.setBackground(Color.GRAY);
-        centerRightPanel.setPreferredSize(new Dimension((int) screenSize.getWidth(),(int) screenSize.getHeight()-150));
-        centerPanel.add(centerRightPanel, BorderLayout.EAST);
+        centerRightPanel.setPreferredSize(new Dimension((int) boardSize.getWidth(),(int) boardSize.getHeight()));
 
+        centerPanel.add(centerLeftPanel, BorderLayout.WEST);
+        centerPanel.add(centerRightPanel, BorderLayout.EAST);
 
         frame.setVisible(true);
     }
